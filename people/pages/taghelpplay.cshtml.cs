2c5f6425983b445bf9a3516d1971a14002510f52
Arkadiusz Lesica <S00182038@mail.itsligo.ie>
2018-10-17 22:32:21 +0100

    TagHelpers

---
+++ people/pages/taghelpplay.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace people.pages
+{
+    public class taghelpplayModel : PageModel
+    {
+        [BindProperty]
+        public string Message { get; set; }
+        public int Number1 { get; set;}
+        public int Number2 { get; set; }
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            int answer = Number1 + Number2;
+            Message = $"Result {answer}";
+        }
+    }
+}
